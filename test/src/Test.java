126da2c6292eb111899ec02b20a6853cf401f305
rayallenbj <bujing01@chinatopcredit.com>
2019-07-30 19:49:17 +0800

    develop commit 2

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("this is develop commit 1");
+        System.out.println("this is develop commit 2");
     }
 }
