c8de526cb5ed7f6298f2631e0ffcb922ae56f455
rayallenbj <bujing01@chinatopcredit.com>
2019-07-30 20:03:18 +0800

    Revert "develop commit 2"
    This reverts commit 126da2c6292eb111899ec02b20a6853cf401f305.
    
    # Conflicts:
    #	test/src/Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("this is develop commit 3");
+        System.out.println("this is develop commit 1");
     }
 }
